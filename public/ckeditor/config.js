4501afa206f35433ffcd953bb5ef72c7b58c2aee
nyaago <nyaago69@me.com>
2011-11-29 09:47:01 +0900

    Adds Ckeditor(Html Editor).

---
+++ public/ckeditor/config.js
@@ -0,0 +1,34 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+//CKEDITOR.config.extraPlugins = 'imageselector';
+//CKEDITOR.config.startupMode = 'source';
+CKEDITOR.config.toolbar =
+[
+['Source','-','Save','Preview'],
+['Cut','Copy','Paste','PasteText','PasteFromWord'],
+ ['Undo','Redo','-','Find','Replace','-','SelectAll'],
+
+'/',
+['Bold','Italic','Underline','Strike'],
+['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+['Link','Unlink','Anchor'],
+['Image','Flash','Table','HorizontalRule','SpecialChar','ImageSelector'],
+'/',
+['Format','Font','FontSize'],
+['TextColor','BGColor'],
+['Maximize', 'ShowBlocks','-','About'],
+];
+
+
+
+
+
+
+
+
+
+
